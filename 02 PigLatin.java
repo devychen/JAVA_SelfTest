d36c248b145f1f8ffa4f261329588dc65602931e
Y CHEN <ychen@Ys-MacBook-Pro.local>
2023-11-29 11:29:46 +0100

    Upload

---
+++ 02 PigLatin.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PigLation2 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter the original lines: ");
+        String originalLine = keyboard.nextLine();
+        String lowerLine = originalLine.toLowerCase(); // all lowercase
+        // no punctuation
+        String result = removePunctuation(input);
+        System.out.println("Result after removing punctuation: " + result);
+    }
+
+    private static String removePunctuation(String input) {
+        // 使用正则表达式移除所有标点符号，并替换为空格
+        return input.replaceAll("[\\p{Punct}&&[^\\s]]", " ");
+
+        
+
+    
+        keyboard.close();
+        
+    }
+}
