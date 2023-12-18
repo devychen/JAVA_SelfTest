bbd6acdb36606261ca597e5d9dabfe55807d435d
Y CHEN <ychen@Ys-MacBook-Pro.local>
2023-12-18 10:49:09 +0100

    Upload

--- 08/STArray.java
+++ 08/STArray.java
@@ -1,3 +1,13 @@
+//Text inside the @literal inline tag is interpreted literally and not as HTML markup.
+//Create a class STArrays which implements two helper methods for arrays. 
+//To display the Javadoc documentation of the class download the Javadoc folder and open the index.html file in the doc folder.
+
+public class STArray {
+    
+}
+
+
+//This is the javadoc comment of the toString() method:
 /**
  * Returns a string representation of the contents of the specified array. 
  * The string representation consists of a list of the array's elements, 
@@ -8,7 +18,9 @@
  * 
  * @return A string representation of the contents of the specified array.
  */
-There is nothing new here, while the comment for slice() has some new tags.
+
+ //There is nothing new here, while the comment for slice() has some new tags.
+
 
 /**
  * Copies the specified range of the specified array into a new array and returns the new array.
@@ -26,5 +38,8 @@ There is nothing new here, while the comment for slice() has some new tags.
  * or<br>
  * {@code start > end}
  */
-This comment uses html <b> tags to format null as bold text and the <br> tag inserts a new line. The{@code} javadoc tag is interpreted literally and not as HTML markup. Everything in the @code braces is inserted directly and the font is set to a typewriter like font.
+
+ //This comment uses html <b> tags to format null as bold text and the <br> tag inserts a new line. 
+ //The{@code} javadoc tag is interpreted literally and not as HTML markup. 
+ //Everything in the @code braces is inserted directly and the font is set to a typewriter like font.
 
